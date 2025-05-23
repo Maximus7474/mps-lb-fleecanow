ffef91fd7995b5164c7eecf2932476060f4ded17
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-23 14:20:43 +0200

    choreui): implement authprovider

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,15 +1,20 @@
 import ReactDOM from 'react-dom/client';
+import { AuthProvider } from './components/provider/AuthProvider';
 import App from './App';
+import { devMode } from './utils/utils';
 
 import './colors.css';
 import './index.css';
-import { devMode } from './utils/utils';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 if (window.name === '' || devMode) {
   const renderApp = () => {
-    root.render(<App />);
+    root.render(
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    );
   };
 
   if (devMode) {
