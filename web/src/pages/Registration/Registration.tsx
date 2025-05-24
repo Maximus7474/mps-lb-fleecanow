11e4668e2cfdf64364e667ce41f7dfbfcd88c93c
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-24 12:16:02 +0200

    feat(Registration): input validation

--- web/src/pages/Registration/Registration.tsx
+++ web/src/pages/Registration/Registration.tsx
@@ -3,23 +3,32 @@ import './Registration.css'
 import { useAuth } from '../../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
 
+type formFields = 'username'|'password'|'confirmedPassword';
+
 const Registration: React.FC = () => {
   const navigate = useNavigate();
   const { user } = useAuth();
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [confirmedPassword, setConfirmedPassword] = useState<string>('');
+  const [invalidFields, setInvalidFields] = useState<Map<formFields, string>>(new Map());
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    
+    if (!validateFields()) return;
     // register(username, password);
   };
 
-  const isLoginValid = () => {
-    if (username.length < 6) return false;
-    if (password.length < 6) return false;
-    if (password !== confirmedPassword) return false;
-    return true;
+  const validateFields = () => {
+    const newInvalidFields = new Map<formFields, string>();
+    if (username.length < 6) newInvalidFields.set('username', 'Username is too short');
+    if (password.length < 6) newInvalidFields.set('password', 'Password is too short');
+    else if (password !== confirmedPassword) newInvalidFields.set('confirmedPassword', 'Password doesn\'t match');
+    
+    setInvalidFields(newInvalidFields);
+
+    return newInvalidFields.size == 0;
   }
 
   useEffect(() => {
@@ -42,33 +51,36 @@ const Registration: React.FC = () => {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
+        <p className='invalid-field'>{invalidFields.get('username')}</p>
       </div>
 
       <div>
-        <label htmlFor='username'>Password:</label>
+        <label htmlFor='password'>Password:</label>
         <input
           type='password'
-          name='confirmedPassword'
+          name='password'
           placeholder='password'
           required
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <p className='invalid-field'>{invalidFields.get('password')}</p>
       </div>
 
       <div>
-          <label htmlFor='username'>Confirm Password:</label>
-          <input
-            type='password'
-            name='confirmedPassword'
-            placeholder='password'
-            required
-            value={confirmedPassword}
-            onChange={(e) => setConfirmedPassword(e.target.value)}
-          />
-        </div>
+        <label htmlFor='confirmedPassword'>Confirm Password:</label>
+        <input
+          type='password'
+          name='confirmedPassword'
+          placeholder='password'
+          required
+          value={confirmedPassword}
+          onChange={(e) => setConfirmedPassword(e.target.value)}
+        />
+        <p className='invalid-field'>{invalidFields.get('confirmedPassword')}</p>
+      </div>
 
-      <button type='submit' className='register-button' disabled={!isLoginValid()}>
+      <button type='submit' className='register-button'>
         Register
       </button>
     </form>
